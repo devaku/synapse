70216d4552ee56b060762e4e8b3e80c2397b8cf6
Alejo Kim Uy <alejokimuy@gmail.com>
2025-09-15 15:20:58 -0600

    Added seed files

--- backend/prisma/migrations/20250915211950_initial_table/migration.sql
+++ backend/prisma/migrations/20250915211950_initial_table/migration.sql
@@ -1,7 +1,9 @@
 -- CreateTable
 CREATE TABLE "synapse_schema"."User" (
     "id" SERIAL NOT NULL,
-    "teamId" INTEGER NOT NULL,
+    "keycloakId" VARCHAR(255) NOT NULL,
+    "username" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
     "firstName" VARCHAR(255),
     "lastName" VARCHAR(255),
     "phone" INTEGER,
@@ -13,10 +15,8 @@ CREATE TABLE "synapse_schema"."User" (
 -- CreateTable
 CREATE TABLE "synapse_schema"."Team" (
     "id" SERIAL NOT NULL,
-    "name" VARCHAR(255),
+    "name" VARCHAR(255) NOT NULL,
     "description" VARCHAR(255),
-    "phone" INTEGER,
-    "userId" INTEGER,
     "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Team_pkey" PRIMARY KEY ("id")
@@ -25,9 +25,9 @@ CREATE TABLE "synapse_schema"."Team" (
 -- CreateTable
 CREATE TABLE "synapse_schema"."Notification" (
     "id" SERIAL NOT NULL,
-    "name" VARCHAR(255),
-    "description" VARCHAR(255),
-    "userId" INTEGER,
+    "name" VARCHAR(255) NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "userId" INTEGER NOT NULL,
     "teamId" INTEGER,
 
     CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
@@ -36,9 +36,9 @@ CREATE TABLE "synapse_schema"."Notification" (
 -- CreateTable
 CREATE TABLE "synapse_schema"."Logs" (
     "id" SERIAL NOT NULL,
-    "name" VARCHAR(255),
-    "description" VARCHAR(255),
-    "userId" INTEGER,
+    "name" VARCHAR(255) NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "userId" INTEGER NOT NULL,
 
     CONSTRAINT "Logs_pkey" PRIMARY KEY ("id")
 );
@@ -48,11 +48,12 @@ CREATE TABLE "synapse_schema"."Task" (
     "id" SERIAL NOT NULL,
     "createdBy" INTEGER NOT NULL,
     "assignedTo" INTEGER NOT NULL,
-    "name" VARCHAR(255),
-    "description" VARCHAR(255),
+    "priority" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
     "image" VARCHAR(255),
-    "startDate" TIMESTAMP(6) NOT NULL,
-    "completeDate" TIMESTAMP(6) NOT NULL,
+    "startDate" TIMESTAMP(6),
+    "completeDate" TIMESTAMP(6),
     "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
@@ -62,23 +63,31 @@ CREATE TABLE "synapse_schema"."Task" (
 CREATE TABLE "synapse_schema"."Comment" (
     "id" SERIAL NOT NULL,
     "userId" INTEGER NOT NULL,
-    "message" VARCHAR(255),
-    "taskId" INTEGER,
+    "message" VARCHAR(255) NOT NULL,
+    "taskId" INTEGER NOT NULL,
 
     CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
 );
 
--- AddForeignKey
-ALTER TABLE "synapse_schema"."Team" ADD CONSTRAINT "Team_userId_fkey" FOREIGN KEY ("userId") REFERENCES "synapse_schema"."User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+-- CreateTable
+CREATE TABLE "synapse_schema"."_TeamToUser" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_TeamToUser_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE INDEX "_TeamToUser_B_index" ON "synapse_schema"."_TeamToUser"("B");
 
 -- AddForeignKey
-ALTER TABLE "synapse_schema"."Notification" ADD CONSTRAINT "Notification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "synapse_schema"."User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "synapse_schema"."Notification" ADD CONSTRAINT "Notification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "synapse_schema"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "synapse_schema"."Notification" ADD CONSTRAINT "Notification_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "synapse_schema"."Team"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "synapse_schema"."Logs" ADD CONSTRAINT "Logs_userId_fkey" FOREIGN KEY ("userId") REFERENCES "synapse_schema"."User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "synapse_schema"."Logs" ADD CONSTRAINT "Logs_userId_fkey" FOREIGN KEY ("userId") REFERENCES "synapse_schema"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "synapse_schema"."Task" ADD CONSTRAINT "Task_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "synapse_schema"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -90,4 +99,10 @@ ALTER TABLE "synapse_schema"."Task" ADD CONSTRAINT "Task_assignedTo_fkey" FOREIG
 ALTER TABLE "synapse_schema"."Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "synapse_schema"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "synapse_schema"."Comment" ADD CONSTRAINT "Comment_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "synapse_schema"."Task"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "synapse_schema"."Comment" ADD CONSTRAINT "Comment_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "synapse_schema"."Task"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "synapse_schema"."_TeamToUser" ADD CONSTRAINT "_TeamToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "synapse_schema"."Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "synapse_schema"."_TeamToUser" ADD CONSTRAINT "_TeamToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "synapse_schema"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,4 +1,14 @@
 // These mirror the same column names in the database
+
+export type userType = {
+	keycloakId: string;
+	teamId: number;
+	firstName?: string;
+	lastName?: string;
+	phone?: number;
+	createdAt: Date;
+};
+
 export type taskType = {
 	createdBy: number;
 	assignedTo: number;
