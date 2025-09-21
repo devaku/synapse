99995267853f21f236aecec62f688a0b0929f8cc
Pedro Gomes <pedrodetoledoh@gmail.com>
2025-09-21 13:51:34 -0600

    feat: Read and Create teams

--- backend/src/services/teams-service.ts
+++ backend/src/services/teams-service.ts
@@ -10,3 +10,22 @@ export async function createTeam(team: teamType){
 
     return teamRow
 }
+
+export async function readAllTeam (){
+    const teamrow = await prisma.team.findMany({
+
+		include: {
+			createdByUser: {
+				select: {
+					id: true,
+					username: true,
+					firstName: true,
+					lastName: true,
+				},
+			}
+        }
+
+    })
+
+    return teamrow
+}
