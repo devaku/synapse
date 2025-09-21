b56333fdac5af3ab74774808a8a1c4839e8aa495
George Sabo <george.sabo@edu.sait.ca>
2025-09-21 14:47:39 -0600

    Update logs.tsx
    WITH THE HELP OF DOCUMENTATION i was able to add the export, its basically copy paste so I cited it.

--- frontend/src/pages/logs.tsx
+++ frontend/src/pages/logs.tsx
@@ -27,6 +27,51 @@ export default function LogsPage() {
 		}
 	}
 
+	// Directly from Documentation
+	// https://react-data-table-component.netlify.app/?path=/docs/examples--exporting-csv
+	const convertArrayOfObjectsToCSV = (array) => {
+		let result: any;
+
+		const columnDelimiter = ',';
+		const lineDelimiter = '\n';
+		const keys = Object.keys(data[0]);
+
+		result = '';
+		result += keys.join(columnDelimiter);
+		result += lineDelimiter;
+
+		array.forEach(item => {
+			let ctr = 0;
+			keys.forEach(key => {
+				if (ctr > 0) result += columnDelimiter;
+
+				result += item[key];
+				ctr++;
+			});
+			result += lineDelimiter;
+		});
+
+		return result;
+	}
+
+	// Directly from Documentation
+	// https://react-data-table-component.netlify.app/?path=/docs/examples--exporting-csv
+	const downloadCSV = (array) => {
+		const link = document.createElement('a');
+		let csv = convertArrayOfObjectsToCSV(array);
+		if (csv == null) return;
+
+		const filename = 'export.csv';
+
+		if (!csv.match(/^data:text\/csv/i)) {
+			csv = `data:text/csv;charset=utf-8,${csv}`;
+		}
+
+		link.setAttribute('href', encodeURI(csv));
+		link.setAttribute('download', filename);
+		link.click();
+	}
+
 	const ExpandedComponent = ({ data }) => (
 		<pre className="w-full whitespace-pre-wrap break-all overflow-hidden text-xs leading-relaxed bg-gray-50 border border-gray-200 p-3">
 			{JSON.stringify(data, null, 2)}
@@ -80,6 +125,9 @@ export default function LogsPage() {
 					</div>
 					<div className={`flex mb-4 w-fit bg-gray-100 p-2 rounded ${display} flex items-center justify-between`}>
 						<span className=''>{selectedRows.length} Selected</span>
+						<button className="py-2 px-3 ml-3 bg-[#153243] text-white border border-[#153243] rounded ml-1" onClick={() => downloadCSV(selectedRows)}>
+							Download CSV
+						</button>
 					</div>
 				</div>
 				<div className="">
