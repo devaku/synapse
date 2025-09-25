d8181fd109bd7c52e9ee2a717da9c636722ac531
Pedro Gomes <pedrodetoledoh@gmail.com>
2025-09-25 17:35:51 -0600

    feat: delete teams working

--- backend/src/routes/teams.ts
+++ backend/src/routes/teams.ts
@@ -2,11 +2,15 @@ import express from 'express';
 import {
 	createTeam,
 	readTeam,
+	deleteTeam,
+	softDeleteTeam
 } from '../controllers/teams-controller';
 
 const teamRouter = express.Router();
 
 teamRouter.get('/teams',readTeam);
 teamRouter.post('/teams', express.json() ,createTeam);
+teamRouter.delete('/teams/', express.json(), deleteTeam);
+teamRouter.patch('/teams/soft-delete/', express.json(), softDeleteTeam);
 
 export default teamRouter; 
