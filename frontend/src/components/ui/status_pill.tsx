3731d89590f20b8a28b387ba188a9498b2f05909
Alejo Kim Uy <alejokimuy@gmail.com>
2025-10-01 14:20:09 -0600

    Added new colors to the pill boxes

--- frontend/src/components/ui/status_pill.tsx
+++ frontend/src/components/ui/status_pill.tsx
@@ -7,38 +7,42 @@ export default function StatusPill({ text }: statusPillProps) {
 	const style = 'text-sm text-center h-7.5 p-1 rounded-xl md:w-24 max-md:w-7';
 	const pillstyle = 'max-md:hidden md:visible';
 	switch (text) {
-		case 'DELIVERED':
+		case 'LOW':
 			finalhtml = (
 				<div className={style + ' bg-green-200 text-green-700'}>
 					<div className={pillstyle}>{text}</div>
 				</div>
 			);
 			break;
-		case 'URGENT':
-		case 'OVERDUE':
+		case 'MEDIUM':
 			finalhtml = (
-				<div className={style + ' bg-red-200 text-red-700'}>
+				<div className={style + ' bg-yellow-200 text-yellow-700'}>
 					<div className={pillstyle}>{text}</div>
 				</div>
 			);
 			break;
-		case 'PENDING':
+
+		case 'HIGH':
 			finalhtml = (
 				<div className={style + ' bg-orange-200 text-orange-700'}>
 					<div className={pillstyle}>{text}</div>
 				</div>
 			);
 			break;
-		case 'READ':
+
+		case 'URGENT':
+		case 'OVERDUE':
+		case 'UNREAD':
 			finalhtml = (
-				<div className={style + ' bg-green-200 text-green-700'}>
+				<div className={style + ' bg-red-200 text-red-700'}>
 					<div className={pillstyle}>{text}</div>
 				</div>
 			);
 			break;
-		case 'UNREAD':
+
+		case 'READ':
 			finalhtml = (
-				<div className={style + ' bg-red-200 text-red-700'}>
+				<div className={style + ' bg-sky-200 text-sky-700'}>
 					<div className={pillstyle}>{text}</div>
 				</div>
 			);
