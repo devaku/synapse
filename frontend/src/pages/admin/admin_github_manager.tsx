90d899cfc3766a8409381bac432cf11a29cb8352
Alejo Kim Uy <alejokimuy@gmail.com>
2025-10-03 19:55:08 -0600

    Moved admin pages to their own folder

--- frontend/src/pages/admin/admin_github_manager.tsx
+++ frontend/src/pages/admin/admin_github_manager.tsx
@@ -1,4 +1,4 @@
-import HeaderContainer from '../components/container/header_container';
+import HeaderContainer from '../../components/container/header_container';
 
 export default function AdminGithubManagerPage() {
 	return (
