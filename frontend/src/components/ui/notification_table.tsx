fe61c68287c9ef878979fc61ad62b360ddb44d7f
Bdopz <theodore.frocklage@edu.sait.ca>
2025-10-20 04:04:11 -0600

    Notif table UI update

--- frontend/src/components/ui/notification_table.tsx
+++ frontend/src/components/ui/notification_table.tsx
@@ -29,7 +29,7 @@ export default function NotificationTable({
 			className="absolute h-100 w-80 mr-39 bg-ttg-white z-100 mt-14 hover:cursor-default border-1 border-ttg-black/15 flex flex-col overflow-y-scroll [scrollbar-width:none]"
 			onClick={(e) => e.stopPropagation()}
 		>
-			<div className="fixed top-13.5 right-21 border-10 border-b-ttg-black/15 border-x-transparent border-t-transparent" />
+			<div className="fixed top-13.5 right-24.5 border-10 border-b-ttg-black/15 border-x-transparent border-t-transparent" />
 			<h2 className="border-b-1 border-ttg-black/15 py-1 px-2 font-semibold fixed w-79.5 bg-ttg-white">
 				Notifications
 			</h2>
