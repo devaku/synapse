dd1d709a3d1df195d309f4d407e6193b023b433f
Joseph Sabo <jsephsabo@gmail.com>
2025-10-28 16:35:37 -0600

    admin task page set up
    import required components for task table, state management for task data, defined table columns for admin task view

--- frontend/src/pages/admin/tasks.tsx
+++ frontend/src/pages/admin/tasks.tsx
@@ -3,21 +3,48 @@ import Button from '../../components/ui/button';
 import DataTable from '../../components/container/DataTableBase';
 import { type TableColumn } from 'react-data-table-component';
 import type { Task } from '../../lib/types/models';
+import { useState } from 'react';
+import StatusPill from '../../components/ui/status_pill';
+import { formatDate } from '../../lib/helpers/datehelpers';
 
 export default function AdminTaskManagerPage() {
+	const [allTaskData, setAllTaskData] = useState<Task[]>([]);
+	const [filteredTasks, setFilteredTasks] = useState<Task[]>([]);
+	const [filterTextTasks, setFilterTextTasks] = useState('');
+
+	const taskColumns: TableColumn<Task>[] = [
+		{
+			name: 'ID',
+			selector: (row) => row.id,
+			sortable: true,
+			width: '80px',
+		},
+		{
+			name: 'Task',
+			selector: (row) => row.name,
+			sortable: true,
+			grow: 2,
+		},
+		{
+			name: 'Created At',
+			selector: (row: Task) => {
+				return formatDate(new Date(row.createdAt!));
+			},
+			sortable: true,
+		},
+		{
+			name: 'Status',
+			selector: (row) => row.priority,
+			sortable: true,
+			cell: (row) => <StatusPill text={row.priority}></StatusPill>,
+		},
+	];
+
 	return (
 		<HeaderContainer pageTitle="Admin - Task Manager">
-			<div>Admin Task Manager Page</div>
-			<div>More to come...</div>
-			<div>
-				This table should just like, display all the task for everyone
+			<div className="flex flex-col">
+				<p>Admin task management interface</p>
 			</div>
-			<div>And have the ability to edit/delete/create tasks</div>
-			<div>
-				Basically everything the normal task page does, but for all
-				users
-			</div>
-			<div>And with more admin controls</div>
 		</HeaderContainer>
 	);
 }
