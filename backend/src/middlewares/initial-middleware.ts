6f03bcce0d735570cbf8ec970d7517fb84288672
Alejo Kim Uy <alejokimuy@gmail.com>
2025-10-31 17:23:47 -0600

    moved CORS and modified public folder

--- backend/src/middlewares/initial-middleware.ts
+++ backend/src/middlewares/initial-middleware.ts
@@ -1,23 +1,14 @@
 import express, { Express } from 'express';
-import cors from 'cors';
+
 import { sessionMiddleware } from './session-middleware';
 import path from 'path';
-import { corsValues } from '../lib/cors';
+
 const PUBLIC_FOLDER = `${path.join(__dirname, '..')}/public`;
 
 export function setupServerMiddleware(app: Express) {
 	// Server static files
-	app.use('/public', express.static(PUBLIC_FOLDER));
+	app.use('/', express.static(PUBLIC_FOLDER));
 
 	// Attach session
 	app.use(sessionMiddleware);
-
-	// Load CORS
-	app.use(
-		cors({
-			// origin: '*',
-			origin: corsValues,
-			credentials: true,
-		})
-	);
 }
